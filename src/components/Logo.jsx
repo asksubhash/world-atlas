456e29d2a8010f9e3756a556baef38d23dd800eb
Subhash Kumar <64916437+asksubhash@users.noreply.github.com>
2024-11-01 11:49:20 +0530

    logo btn click enabled

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,19 @@
-import { TiWorld } from "react-icons/ti"
+import { TiWorld } from "react-icons/ti";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div className=" flex gap-2 items-center ">
+    <Link to={`/`} aria-label="Navigate to World Atlas" className="flex gap-2 items-center ">
     <div className={`bg-main p-[6px] rounded-md`}>
-      <TiWorld className=" text-white text-3xl" />
+      <TiWorld className="text-white text-3xl" />
     </div>
-    <div className=" flex gap-1 text-[19px]">
-      <span className={`font-bold text-main `}>World</span>
+    <div className="flex gap-1 text-[19px]">
+      <span className={`font-bold text-main`}>World</span>
       <span className="text-slate-400">Atlas</span>
     </div>
-  </div>
-  )
-}
+  </Link>
+  
+  );
+};
 
-export default Logo
+export default Logo;
